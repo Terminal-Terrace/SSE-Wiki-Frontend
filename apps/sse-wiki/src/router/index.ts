64ef74550771b13b36331f59cfe45a7a247dfa0a
jerrylingj <498539230@qq.com>
2025-10-03 09:13:15 +0800

    feat: 移除 sse-wiki 内的 auth 模块

--- apps/sse-wiki/src/router/index.ts
+++ apps/sse-wiki/src/router/index.ts
@@ -30,28 +30,6 @@ const router = createRouter({
         },
       ],
     },
-    {
-      path: '/auth',
-      name: 'auth',
-      component: () => import('@/layouts/AuthLayout.vue'),
-      children: [
-        {
-          path: 'login',
-          name: 'login',
-          component: () => import('@/views/auth/LoginView.vue'),
-        },
-        {
-          path: 'register',
-          name: 'register',
-          component: () => import('@/views/auth/RegisterView.vue'),
-        },
-        {
-          path: 'reset',
-          name: 'reset',
-          component: () => import('@/views/auth/ResetView.vue'),
-        },
-      ],
-    },
   ],
 })
 
