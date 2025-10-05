23ce10050940db3e7082767eec2df1e53f505b43
jerrylingj <498539230@qq.com>
2025-10-05 19:52:00 +0800

    feat: 引入Toast组件

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -16,6 +16,7 @@ export * from './components/ui/sidebar'
 export * from './components/ui/skeleton'
 export * from './components/ui/tabs'
 export * from './components/ui/textarea'
+export * from './components/ui/toast'
 export * from './components/ui/tooltip'
 
 export { cn } from './lib/utils'
