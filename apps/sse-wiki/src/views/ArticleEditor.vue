3fe2814a49e456d650ba5974b132d6904e4cee68
math-zhuxy <235934429@qq.com>
2025-10-15 16:43:06 +0800

    feat: add editor
    use tiptap component to add a editor

--- apps/sse-wiki/src/views/ArticleEditor.vue
+++ apps/sse-wiki/src/views/ArticleEditor.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { toRefs } from 'vue'
 import { useRoute } from 'vue-router'
+import articleEdit from '@/components/edit/articleEdit.vue'
 
 interface Props {
   articleId?: string
@@ -14,7 +15,7 @@ const moduleId = route.query.moduleId as string
 </script>
 
 <template>
-  <div class="p-6">
+  <div>
     <div class="mx-auto max-w-5xl">
       <h1 class="text-3xl font-bold text-gray-900 mb-6">
         文章编辑器
@@ -30,9 +31,7 @@ const moduleId = route.query.moduleId as string
           所属模块ID: {{ moduleId }}
         </p>
       </div>
-      <p class="text-gray-500 italic">
-        这是文章编辑器页面的占位符，等待后续实现。
-      </p>
     </div>
+    <articleEdit />
   </div>
 </template>
