3489fd26a0b7994549d8d4030240fad29456eb58
jerrylingj <498539230@qq.com>
2025-12-31 10:12:35 +0800

    refactor(editor): remove legacy editor components
    Remove old ContentEditor and related components:
    - ContentEditor.vue
    - EditorToolbar.vue
    - FileCard.vue
    - ImageCard.vue
    - UploadProgress.vue
    - All composables and utilities
    
    Replaced by: @sse-wiki/vue-rich-editor package
    
    Related: #008-comprehensive-code-audit

--- apps/sse-wiki/src/components/common/editor/composables/index.ts
+++
@@ -1,5 +0,0 @@
-/**
- * 编辑器组合式函数导出
- */
-export { useFileUpload } from './useFileUpload'
-export type { UploadTask, UploadTaskStatus, UseFileUploadOptions, UseFileUploadReturn } from './useFileUpload'
